296cb9e8409d6c9b796461985d6ca75d5c3cc385
Linsey <linsey.whitlock@asynchrony.com>
2017-02-26 21:48:07 -0600

    tutorial chapters 1 & 2

---
+++ server.js
@@ -0,0 +1,17 @@
+import config from './config';
+
+import express from 'express';
+const server = express();
+
+server.set('view engine', 'ejs');
+
+server.get('/', (req, res) => {
+  res.render('index');
+});
+
+// server.use('/api', apiRouter);
+server.use(express.static('public'));
+
+server.listen(config.port, () =>{
+  console.info('express listening on port ', config.port);
+});
